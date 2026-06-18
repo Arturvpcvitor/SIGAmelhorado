ea7cb5cbafc4c7a847f18d347bd0b6d37ef43967
Arturvpcvitor <arturvpcvitor@gmail.com>
2026-06-17 22:53:17 -0300

    Update WebConfig.java

--- src/main/java/com/example/aulabd/config/WebConfig.java
+++ src/main/java/com/example/aulabd/config/WebConfig.java
@@ -1,1 +1,15 @@
 
+// WebConfig.java
+package com.example.aulabd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor());
+    }
+}
