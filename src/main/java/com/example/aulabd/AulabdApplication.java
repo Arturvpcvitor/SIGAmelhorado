ae0a1d824df82ce31c7338ff50435512ed6fe19d
Arturvpcvitor <arturvpcvitor@gmail.com>
2026-06-17 22:40:35 -0300

    Update AulabdApplication.java

--- src/main/java/com/example/aulabd/AulabdApplication.java
+++ src/main/java/com/example/aulabd/AulabdApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AulabdApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AulabdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AulabdApplication.class, args);
+    }
 }
