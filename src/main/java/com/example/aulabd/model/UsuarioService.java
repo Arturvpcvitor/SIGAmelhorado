e41015e9170706410672c48ebfb507ee4d56f6b9
Arturvpcvitor <arturvpcvitor@gmail.com>
2026-06-17 22:52:24 -0300

    Update UsuarioService.java

--- src/main/java/com/example/aulabd/model/UsuarioService.java
+++ src/main/java/com/example/aulabd/model/UsuarioService.java
@@ -1,1 +1,25 @@
 
+// UsuarioService.java
+package com.example.aulabd.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    UsuarioDAO usuarioDAO;
+
+    public Usuario autenticar(String login, String senha) {
+        Usuario user = usuarioDAO.buscarPorLogin(login);
+        if (user != null && user.getSenha().equals(senha)) {
+            return user;
+        }
+        return null;
+    }
+
+    public void criarUsuario(Usuario usuario) {
+        usuarioDAO.inserir(usuario);
+    }
+}
