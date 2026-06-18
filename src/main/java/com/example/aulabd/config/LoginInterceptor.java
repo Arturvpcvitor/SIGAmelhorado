2ed2878e186b49fdaae73ca85bfd3b8710fb5f3e
Arturvpcvitor <arturvpcvitor@gmail.com>
2026-06-17 22:53:01 -0300

    Update LoginInterceptor.java

--- src/main/java/com/example/aulabd/config/LoginInterceptor.java
+++ src/main/java/com/example/aulabd/config/LoginInterceptor.java
@@ -1,1 +1,28 @@
 
+// LoginInterceptor.java
+package com.example.aulabd.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        String uri = request.getRequestURI();
+        
+        if (uri.equals("/login") || uri.equals("/registrar") || uri.equals("/favicon.ico") || uri.startsWith("/css") || uri.startsWith("/js")) {
+            return true;
+        }
+        
+        if (session.getAttribute("usuario") == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
+}
