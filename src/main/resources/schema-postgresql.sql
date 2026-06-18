b4d9763c1a06146cd7c89e964b1382604070a792
Arturvpcvitor <arturvpcvitor@gmail.com>
2026-06-17 22:50:33 -0300

    Update schema-postgresql.sql

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -3,4 +3,10 @@ CREATE TABLE IF NOT EXISTS aluno(
     nome VARCHAR(100) NOT NULL,
     cpf  VARCHAR(11)  NOT NULL 
 );
+CREATE TABLE IF NOT EXISTS usuario(
+    login VARCHAR(50) PRIMARY KEY,
+    senha VARCHAR(100) NOT NULL
+);
 
+-- Insere um usuário padrão para teste
+INSERT INTO usuario (login, senha) VALUES ('admin', '1234') ON CONFLICT (login) DO NOTHING;
